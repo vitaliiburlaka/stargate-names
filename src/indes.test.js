002989ff5b640312c75332a60b5a251923721f37
Vitalii Burlaka <vitalii.burlaka@gmail.com>
2019-01-05 19:23:31 +0100

    feat(random): Add abillity to get an array of random Stargate names
    If you pass a number to the random function, you will receive an array with that number of random
    Stargate names

--- src/indes.test.js
+++ src/indes.test.js
@@ -1,15 +1,15 @@
 var stargate = require('./index')
 
 describe('stargate-names', function() {
+  function isArrayOfStrings(array) {
+    return array.every(function(item) {
+      return typeof item === 'string'
+    })
+  }
+
   describe('all', function() {
     it('should be an array of strings', function() {
       expect(isArrayOfStrings(stargate.all)).toEqual(true)
-
-      function isArrayOfStrings(array) {
-        return array.every(function(item) {
-          return typeof item === 'string'
-        })
-      }
     })
 
     it('should contain `Teal\'c`', function() {
@@ -20,7 +20,18 @@ describe('stargate-names', function() {
   describe('random', function() {
     it('should return a random item from the stargate.all', function() {
       var randomItem = stargate.random()
+
       expect(stargate.all).toContain(randomItem)
+    })
+
+    it('should return an array of random items if passed a number', function() {
+      var randomItems = stargate.random(3)
+
+      expect(isArrayOfStrings(randomItems)).toEqual(true)
+      expect(randomItems).toHaveLength(3)
+      randomItems.forEach(function(item) {
+        expect(stargate.all).toContain(item)
+      })
     })
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 var uniqueRandomArray = require('unique-random-array')
 var stargateNames = require('./stargate-names.json')
+var getRandomItem = uniqueRandomArray(stargateNames)
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem()
+  } else {
+    var randomItems = []
+    for (var i = 0; i < number; i++) {
+      randomItems.push(getRandomItem())
+    }
+
+    return randomItems
+  }
+}
 
 module.exports = {
   all: stargateNames,
-  random: uniqueRandomArray(stargateNames)
+  random: random
 }
