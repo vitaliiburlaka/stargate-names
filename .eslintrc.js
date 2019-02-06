5d4258db529ccfe8e4ffc7c8068a54a7ae054aa1
Vitalii Burlaka <vitalii.burlaka@gmail.com>
2019-02-06 17:49:37 +0100

    style(Prettier): Prettified

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,13 +18,13 @@ module.exports = {
   rules: {
     'prettier/prettier': 2,
 
-    'eqeqeq': [ 2, 'allow-null' ],
+    eqeqeq: [2, 'allow-null'],
 
     'no-console': 0,
     'no-shadow': 2,
     'no-undef': 2,
     'no-unused-expressions': 0,
-    'no-unused-vars': [ 2, { 'ignoreRestSiblings': true } ],
+    'no-unused-vars': [2, { ignoreRestSiblings: true }],
     'no-var': 2,
     'no-redeclare': 2,
 
@@ -41,20 +41,7 @@ module.exports = {
     'prefer-rest-params': 2,
     'prefer-spread': 2,
     'prefer-template': 2,
-    'template-curly-spacing': [ 2, 'never' ],
 
-    'object-curly-spacing': [ 2, 'always' ],
-    'quote-props': [ 2, 'consistent' ],
-
-    'semi': [ 2, 'never' ],
-    'space-before-blocks': [ 2, 'always' ],
-    'newline-per-chained-call': [
-      2,
-      {
-        ignoreChainWithDepth: 2,
-      },
-    ],
-    'spaced-comment': [ 2, 'always' ],
-    'keyword-spacing': 2,
+    'quote-props': [2, 'consistent'],
   },
 }
